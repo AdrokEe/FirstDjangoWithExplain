e40c796798a521836c588acc58239c9fc1b8cc1e
Adroke <adroke@163.com>
2023-06-06 20:07:57 +0800

    add deploy suggest

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 <h1>这是一个参照Django官方文档所写的示例项目</h1>
-<br>
+项目 Django 版本: 4.1.1<br>
 <h3>修改将以注释的形式所记录下来，控制台所使用的命令也将包含在内</h3>
 <p>本项目不会讲解前端和数据库部分的内容</p>
+<p>会根据个人经验进行一些修改变动，不完全忠于文档内容</p>
 <p><u>WhatHappen.md记录了除编写代码以外需要做的事情</u></p>
 <br>
 阅读代码时请参照此文档: 
 
-[官方文档](https://docs.djangoproject.com/zh-hans/4.2/intro/tutorial01/)
+[官方文档(v4.2)](https://docs.djangoproject.com/zh-hans/4.2/intro/tutorial01/)
--- django_document_project/polls/admin.py
+++ django_document_project/polls/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
 # 2.6 在此处注册需要显示在后台管理界面的模型
-admin.site.register(Question)
+# admin.site.register(Question)
+
+# 7.1 创建一个模型后台类再进行注册，观察 admin 怎么工作的
+# 访问管理后台并查看 Question 模型，字段将会按照我们指定的顺序排序
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # 对字段指定显示顺序
+    # fields = ["pub_date", "question_text"]
+
+    # 也可以对字段进行分组
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]})
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
+
+# 7.3 注册 Choice 模型观察如何显示关联对象
