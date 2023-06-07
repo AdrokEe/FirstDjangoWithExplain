b383f78c9c92a919391dae817807619fe5e3d948
Adroke <adroke@163.com>
2023-06-07 22:24:06 +0800

    part 7 -75%

--- django_document_project/polls/models.py
+++ django_document_project/polls/models.py
@@ -1,7 +1,9 @@
 import datetime
-
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
+# 注意!
+# 每当对模型类进行任何修改后，都请记得对数据库进行迁移
 
 
 # 2.3 建立两个模型 Question 和 Choice
@@ -18,7 +20,7 @@ class Question(models.Model):
     # 如果未指定主键，将会自动生成名为 id 的主键
     # 如果未指定字段名，将会自动生成'模型名_变量名'的字段名
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateField(verbose_name="date published")
+    pub_date = models.DateTimeField(verbose_name="date published")
 
     # 修改当访问该对象时返回的内容以便之后在命令台查看（此类方法为 Python 中所有类都可以重写的魔术方法）
     def __str__(self):
@@ -30,6 +32,18 @@ class Question(models.Model):
 
     # 5.2 修改后的方法
     # 回到 tests.py 中添加更全面的测试  > 5.3
+    # def was_published_recently(self):
+    #     now = timezone.now()
+    #     return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    # 7.5 使用 display()修饰器改进显示
+    # 详见：https://docs.djangoproject.com/zh-hans/4.2/ref/contrib/admin/#django.contrib.admin.ModelAdmin.list_display
+    # 回到 admin.py  > 7.6
+    @admin.display(
+        boolean=True,  # 将布尔值转化为图标
+        ordering="pub_date",  # 按照哪个字段排序
+        description="Published recently?",  # 列名显示
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
